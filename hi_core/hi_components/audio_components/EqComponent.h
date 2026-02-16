fc4882cacfe847ae2d57a914c1ae81209d374c37
Michael Zajner <mikezajner@gmail.com>
2026-02-16 08:13:17 -0500

    limit band amount on EQ component

--- hi_core/hi_components/audio_components/EqComponent.h
+++ hi_core/hi_components/audio_components/EqComponent.h
@@ -43,193 +43,196 @@ class FilterDragOverlay : public Component,
 	public Timer
 {
 public:
-
-	
-
-	enum class SpectrumVisibility
-	{
-		Dynamic,
-		AlwaysOn,
-		AlwaysOff
-	};
-
-	using DragAction = ProcessorFilterStatistics::DragAction;
-
-	struct DragData
-	{
-		DragData() = default;
-
-		bool selected;
-		bool enabled;
-		bool dragging;
-		bool hover;
-		float frequency;
-		float q;
-		float gain;
-		String type;
-	};
-
-	struct LookAndFeelMethods
-	{
-		virtual ~LookAndFeelMethods() {};
-		virtual void drawFilterDragHandle(Graphics& g, FilterDragOverlay& o, int index, Rectangle<float> handleBounds, const DragData& d);
-	};
-
-	struct Factory : public PathFactory
-	{
-		String getId() const override { return "FilterIcons"; }
-		Path createPath(const String& url) const override;
-	};
-
-	enum ColourIds
-	{
-		bgColour = 125160,
-		textColour
-	};
-
-	struct Panel;
-
-	int offset = 12;
-
-	struct Listener
-	{
-		virtual ~Listener() {};
-
-		virtual void bandRemoved(int index) = 0;
-		virtual void filterBandSelected(int index) = 0;
-
-		JUCE_DECLARE_WEAK_REFERENCEABLE(Listener);
-	};
-
-	
-
-
-	
-
-	struct FilterResizeAction : public UndoableAction
-	{
-		FilterResizeAction(ProcessorFilterStatistics* stats, int index_, bool add, double freq_=0.0, double gain_=0.0);;
-
-		bool perform() override;
-		bool undo() override;
-
-		ProcessorFilterStatistics::Ptr stats;
-		int index;
-		bool isAddAction;
-
-		double freq;
-		double gain;
-		int type;
-		double q;
-		bool enabled;
-	};
-
-	FilterDragOverlay(Processor* eq_, ProcessorFilterStatistics::Ptr statistics, bool isInFloatingTile_ = false);
-	virtual ~FilterDragOverlay();
-
-	void otherChange(Processor* p) override
-	{
-		checkEnabledBands();
-		updateFilters();
-		updatePositions(true);
-	}
-
-	void checkEnabledBands();
-
-	void timerCallback() override;
-	void resized() override;
-	void paint(Graphics &g);
-	void paintOverChildren(Graphics& g);
-
-	void mouseMove(const MouseEvent &e);
-	void mouseDown(const MouseEvent &e);
-	void mouseDrag(const MouseEvent &e) override;
-	void mouseUp(const MouseEvent& e);
-
-	void addFilterDragger(int index);
-	void updateFilters();
-	void updateCoefficients();
-	void addFilterToGraph(int filterIndex, int filterType);
-	void updatePositions(bool forceUpdate);
-	Point<int> getPosition(int index);
-
-	void lookAndFeelChanged() override;
-
-	bool shouldResetOnDoubleClick() const noexcept { return resetOnDoubleClick; }
-
-	void setResetOnDoubleClick(bool shouldReset) { resetOnDoubleClick = shouldReset; }
-
-	void setAllowContextMenu(bool shouldAllow)
-	{
-		allowContextMenu = shouldAllow;
-	}
-
-	void setGainRange(double maxGain)
-	{
-		gainRange = jlimit(1.0, 36.0, maxGain);
-		filterGraph.setGainRange(maxGain);
-	}
-
-	virtual void fillPopupMenu(PopupMenu& m, int handleIndex);
-
-	virtual void popupMenuAction(int result, int handleIndex);
-
-	Font font;
-	bool isInFloatingTile = false;
-	ScopedPointer<LookAndFeel> plaf;
-
-	class FilterDragComponent : public Component
-	{
-	public:
-
-		FilterDragComponent(FilterDragOverlay& parent_, int index_);;
-
-		void setConstrainer(ComponentBoundsConstrainer *constrainer_);;
-
-		void mouseEnter(const MouseEvent& e);
-		void mouseExit(const MouseEvent& e);
-		void mouseDown(const MouseEvent& e);
-		void mouseUp(const MouseEvent& e);
-		void mouseDrag(const MouseEvent& e);
-		void mouseWheelMove(const MouseEvent &e, const MouseWheelDetails &d) override;
-		void mouseDoubleClick(const MouseEvent& e);
-		void setSelected(bool shouldBeSelected);
-		void paint(Graphics &g);;
-		void setIndex(int newIndex);;
-
-		bool isSelected() const { return selected; }
-		bool isDragging() const { return !menuActive && (draggin || down); }
-		bool isOver() const { return !menuActive && over; }
-		int getIndex() const { return index; }
-
-	private:
-
-		bool down = false;
-		bool over = false;
-		bool draggin = false;
-		int index;
-		bool selected;
-		bool menuActive = false;
-
-		float dragQStart = 1.0f;
-
-		ComponentBoundsConstrainer *constrainer;
-		ComponentDragger dragger;
-		FilterDragOverlay& parent;
-	};
-
-	void removeFilter(int index);
-
-	double getGain(int y);
-
-	void selectDragger(int index, NotificationType n = sendNotificationSync);
-	void addListener(Listener* l);
-	void removeListener(Listener* l);
-
-	void setAllowFilterResizing(bool shouldBeAllowed);
-	void setSpectrumVisibility(SpectrumVisibility m);
-	void setUndoManager(UndoManager* newUndoManager);
-	void setEqAttribute(int b, int filterIndex, float value);
+    
+    
+    
+    enum class SpectrumVisibility
+    {
+        Dynamic,
+        AlwaysOn,
+        AlwaysOff
+    };
+    
+    using DragAction = ProcessorFilterStatistics::DragAction;
+    
+    struct DragData
+    {
+        DragData() = default;
+        
+        bool selected;
+        bool enabled;
+        bool dragging;
+        bool hover;
+        float frequency;
+        float q;
+        float gain;
+        String type;
+    };
+    
+    struct LookAndFeelMethods
+    {
+        virtual ~LookAndFeelMethods() {};
+        virtual void drawFilterDragHandle(Graphics& g, FilterDragOverlay& o, int index, Rectangle<float> handleBounds, const DragData& d);
+    };
+    
+    struct Factory : public PathFactory
+    {
+        String getId() const override { return "FilterIcons"; }
+        Path createPath(const String& url) const override;
+    };
+    
+    enum ColourIds
+    {
+        bgColour = 125160,
+        textColour
+    };
+    
+    struct Panel;
+    
+    int offset = 12;
+    
+    struct Listener
+    {
+        virtual ~Listener() {};
+        
+        virtual void bandRemoved(int index) = 0;
+        virtual void filterBandSelected(int index) = 0;
+        
+        JUCE_DECLARE_WEAK_REFERENCEABLE(Listener);
+    };
+    
+    
+    
+    
+    
+    
+    struct FilterResizeAction : public UndoableAction
+    {
+        FilterResizeAction(ProcessorFilterStatistics* stats, int index_, bool add, double freq_=0.0, double gain_=0.0);;
+        
+        bool perform() override;
+        bool undo() override;
+        
+        ProcessorFilterStatistics::Ptr stats;
+        int index;
+        bool isAddAction;
+        
+        double freq;
+        double gain;
+        int type;
+        double q;
+        bool enabled;
+    };
+    
+    FilterDragOverlay(Processor* eq_, ProcessorFilterStatistics::Ptr statistics, bool isInFloatingTile_ = false);
+    virtual ~FilterDragOverlay();
+    
+    void otherChange(Processor* p) override
+    {
+        checkEnabledBands();
+        updateFilters();
+        updatePositions(true);
+    }
+    
+    void checkEnabledBands();
+    
+    void timerCallback() override;
+    void resized() override;
+    void paint(Graphics &g);
+    void paintOverChildren(Graphics& g);
+    
+    void mouseMove(const MouseEvent &e);
+    void mouseDown(const MouseEvent &e);
+    void mouseDrag(const MouseEvent &e) override;
+    void mouseUp(const MouseEvent& e);
+    
+    void addFilterDragger(int index);
+    void updateFilters();
+    void updateCoefficients();
+    void addFilterToGraph(int filterIndex, int filterType);
+    void updatePositions(bool forceUpdate);
+    Point<int> getPosition(int index);
+    
+    void lookAndFeelChanged() override;
+    
+    bool shouldResetOnDoubleClick() const noexcept { return resetOnDoubleClick; }
+    
+    void setResetOnDoubleClick(bool shouldReset) { resetOnDoubleClick = shouldReset; }
+    
+    void setAllowContextMenu(bool shouldAllow)
+    {
+        allowContextMenu = shouldAllow;
+    }
+    
+    void setGainRange(double maxGain)
+    {
+        gainRange = jlimit(1.0, 36.0, maxGain);
+        filterGraph.setGainRange(maxGain);
+    }
+    
+    virtual void fillPopupMenu(PopupMenu& m, int handleIndex);
+    
+    virtual void popupMenuAction(int result, int handleIndex);
+    
+    Font font;
+    bool isInFloatingTile = false;
+    ScopedPointer<LookAndFeel> plaf;
+    
+    class FilterDragComponent : public Component
+    {
+    public:
+        
+        FilterDragComponent(FilterDragOverlay& parent_, int index_);;
+        
+        void setConstrainer(ComponentBoundsConstrainer *constrainer_);;
+        
+        void mouseEnter(const MouseEvent& e);
+        void mouseExit(const MouseEvent& e);
+        void mouseDown(const MouseEvent& e);
+        void mouseUp(const MouseEvent& e);
+        void mouseDrag(const MouseEvent& e);
+        void mouseWheelMove(const MouseEvent &e, const MouseWheelDetails &d) override;
+        void mouseDoubleClick(const MouseEvent& e);
+        void setSelected(bool shouldBeSelected);
+        void paint(Graphics &g);;
+        void setIndex(int newIndex);;
+        
+        bool isSelected() const { return selected; }
+        bool isDragging() const { return !menuActive && (draggin || down); }
+        bool isOver() const { return !menuActive && over; }
+        int getIndex() const { return index; }
+        
+    private:
+        
+        bool down = false;
+        bool over = false;
+        bool draggin = false;
+        int index;
+        bool selected;
+        bool menuActive = false;
+        
+        float dragQStart = 1.0f;
+        
+        ComponentBoundsConstrainer *constrainer;
+        ComponentDragger dragger;
+        FilterDragOverlay& parent;
+    };
+    
+    void removeFilter(int index);
+    
+    double getGain(int y);
+    
+    void selectDragger(int index, NotificationType n = sendNotificationSync);
+    void addListener(Listener* l);
+    void removeListener(Listener* l);
+    
+    void setAllowFilterResizing(bool shouldBeAllowed);
+    void setSpectrumVisibility(SpectrumVisibility m);
+    void setUndoManager(UndoManager* newUndoManager);
+    void setEqAttribute(int b, int filterIndex, float value);
+    void setMaxBands(int newMaxBands) { maxBands = jmax(0, newMaxBands); }
+    int getMaxBands() const  { return maxBands; }
+    bool canAddMoreBands() const;
 
 protected:
 
@@ -264,6 +267,7 @@ private:
 
 	bool resetOnDoubleClick = false;
 	bool allowFilterResizing = true;
+    int maxBands = 0; // unlimited
 	SpectrumVisibility fftVisibility = SpectrumVisibility::Dynamic;
 	LookAndFeelMethods defaultLaf;
 	Array<WeakReference<Listener>> listeners;
