6f72001fa0cf2d7681043e75e58ca6e10b483fff
Michael Zajner <mikezajner@gmail.com>
2026-04-16 08:04:39 -0400

    - added Rest API

---
+++ hi_backend/backend/ai_tools/include_rest_server.cpp
@@ -0,0 +1,7 @@
+/*
+    This file ensures RestServer and RestHelpers are compiled as part of the hi_backend module.
+    It simply includes the implementation files directly to force compilation.
+*/
+
+#include "RestServer.cpp"
+#include "RestHelpers.cpp"
